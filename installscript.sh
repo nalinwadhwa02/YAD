6d2c969d6e893033dd0a2c0bf354e51f6eae3116
nalinwadhwa02 <nalin.wadhwa02@gmail.com>
2021-12-28 23:47:57 +0530

    added i3 and configs

--- installscript.sh
+++ installscript.sh
@@ -98,6 +98,7 @@ grub-mkconfig -o /boot/grub/grub.cfg
 pacman -Syu --noconfirm vim htop neofetch xorg xorg-xinit mesa i3 kitty dmenu unzip lxappearance nnn feh htop mpv scrot lxsession lightdm firefox pulseaudio pulseaudio-bluetooth pulseaudio-alsa picom networkmanager network-manager-applet udiskie bluez blueman dunst unclutter xfce4-power-manager
 systemctl enable lightdm.service
 systemctl enable bluetooth.service
+mkdir -p /home/${user}/.config/i3
 curl https://raw.githubusercontent.com/nalinwadhwa02/dotfiles/main/i3/config >> /home/${user}/.config/i3/config
 curl https://raw.githubusercontent.com/nalinwadhwa02/dotfiles/main/autostart.sh >> /home/${user}/.config/autostart.sh
 chmod 700 /home/${user}/.config/autostart.sh
